e50449fe4a8b649eef17cbe6178cdfd1dd9732a7
alexisoller <alexisolleraw@gmail.com>
2020-02-13 16:48:39 +0100

    tesdt
    test

---
+++ Assets/Scripts/Move_Sphere.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Move_Sphere : MonoBehaviour {
+    // Start is called before the first frame update
+
+    public float speed;
+    private float mov = 0;
+    private bool izDer = false;
+    void Start() {
+
+
+    }
+
+    // Update is called once per frame
+    void Update() {
+
+        transform.Translate(mov*speed*Time.deltaTime, 0,0);
+
+        float x = transform.position.x;
+
+        if (!izDer) {
+            mov = 1;
+            if (x > 40) {
+                izDer = true;
+            }
+        } else {
+            mov = -1;
+            if (x < 0) {
+                izDer = false;
+            }
+        }
+        Debug.Log(x);
+        Debug.Log(izDer);
+
+
+    }
+}
