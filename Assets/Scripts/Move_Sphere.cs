b8a0bd3d8073d7a7de10ab17ca1089600be9a31b
alexisoller <alexisolleraw@gmail.com>
2020-02-13 17:08:16 +0100

    final
    final eficiente

--- Assets/Scripts/Move_Sphere.cs
+++ Assets/Scripts/Move_Sphere.cs
@@ -9,17 +9,13 @@ public class Move_Sphere : MonoBehaviour {
     private float mov = 0;
     private bool izDer = false;
     void Start() {
-
-
     }
-
     // Update is called once per frame
     void Update() {
-
-        transform.Translate(mov*speed*Time.deltaTime, 0,0);
-
+        //transform.Translate(mov*speed*Time.deltaTime, 0,0);
+        transform.Translate(speed * Time.deltaTime, 0, 0);
         float x = transform.position.x;
-
+        /*
         if (!izDer) {
             mov = 1;
             if (x > 40) {
@@ -30,10 +26,16 @@ public class Move_Sphere : MonoBehaviour {
             if (x < 0) {
                 izDer = false;
             }
+        }*/
+
+        if (x > 40) {
+            speed=speed * -1;
         }
-        Debug.Log(x);
-        Debug.Log(izDer);
+        else if (x < 0) {
+            speed = speed * -1;
+        }
 
-
+       // Debug.Log(x);
+       // Debug.Log(izDer);
     }
 }
